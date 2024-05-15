c66a3e926158afc09f110be0096d9ec9cad510f5
blueapple825 <blueapple8259@gmail.com>
2024-05-15 01:58:09 +0000

    add bytecode functions

--- bytecode.h
+++ bytecode.h
@@ -14,6 +14,10 @@ typedef struct _Bytecode
     int realLength;
 }Bytecode;
 
+Bytecode* createBytecode(const char* code);
+void setBytecode(Bytecode* bytecode, const char* code);
+void addBytecode(Bytecode* bytecode, const char* code);
+
 void tokenToBytecode(TokenList* tokenList);
 
 #endif
